dbb2cb092130a2b21ec5d112c72c7447d77b9e76
Gregg Luskin <gdluskin@gmail.com>
2023-12-14 13:41:05 -0800

    use dotenv

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,18 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+dotenv.config({
+  path: '.env',
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(5000);
+  await app.listen(process.env.PORT as string);
+
+  console.log(
+    `Application is running on: http://localhost:${process.env.PORT}`,
+  );
 }
 bootstrap();
