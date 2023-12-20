eceaad58e1ad41ef34227f9d250bace21823b43f
Gregg Luskin <gdluskin@gmail.com>
2023-12-20 15:59:20 -0800

    delete it

--- packages/ui/src/components/button/button.component.tsx
+++ packages/ui/src/components/button/button.component.tsx
@@ -1,5 +1,12 @@
+import stylex from "@stylexjs/stylex"
 import { ButtonProps } from "./button.props"
 
 export default function Button(props: ButtonProps) {
-  return <button>{props.children}</button>
+  return <button {...stylex.props(style.main)}>{props.children}</button>
 }
+
+const style = stylex.create({
+  main: {
+    color: "red",
+  },
+})
