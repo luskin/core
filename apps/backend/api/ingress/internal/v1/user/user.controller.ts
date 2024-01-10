8ff877b86e597a6fc20548e156aba738bd7ab7ee
Gregg Luskin <gdluskin@gmail.com>
2024-01-10 15:01:17 -0800

    init

--- apps/backend/api/ingress/internal/v1/user/user.controller.ts
+++ apps/backend/api/ingress/internal/v1/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Inject, Param, Post } from '@nestjs/common';
 import { User, UserService } from '@modules/user';
-import { RequestUser } from 'api/decorators/request-user.decorator';
 import logger from '@libs/logger';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('v1/user')
 export class InternalApiV1UserController {
@@ -14,6 +14,10 @@ export class InternalApiV1UserController {
   }
 
   @Get(':id')
+  @ApiResponse({
+    type: User,
+    description: 'Get a user by id',
+  })
   async getById(@Param('id') id: string): Promise<User> {
     logger.log('The id is: ', id);
     return await this.userService.getById(id);
--- apps/backend/modules/user/domain/user.entity.ts
+++ apps/backend/modules/user/domain/user.entity.ts
@@ -1,9 +1,12 @@
-export class User {
-  id: string;
-  createdAt: Date;
-  updatedAt: Date;
+import { Entity, api } from '@mothership/nest';
+
+export class User extends Entity {
+  @api.IsString()
   firstName: string;
+  @api.IsString()
   lastName: string;
+  @api.IsEmail()
   email: string;
+  @api.IsOptionalString()
   firebaseAuthId: string;
 }
