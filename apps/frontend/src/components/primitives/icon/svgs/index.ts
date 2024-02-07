2766274d03825ffe270f3b858ab819ab7029d39c
Gregg Luskin <gdluskin@gmail.com>
2024-02-07 14:22:04 -0800

    us txMerge

--- apps/frontend/src/components/primitives/icon/svgs/index.ts
+++ apps/frontend/src/components/primitives/icon/svgs/index.ts
@@ -1,8 +1,17 @@
 import alienShip from "./alien-ship.svg"
 import claimsShield from "./claims-shield.svg"
 import wordmark from "./mothership-wordmark.svg"
+import history from "./history.svg"
 import trucks from "./trucks.svg"
 import locationArrow from "./location-arrow.svg"
 import paymentCard from "./payment-card.svg"
 
-export { alienShip, claimsShield, locationArrow, trucks, paymentCard, wordmark }
+export {
+  alienShip,
+  claimsShield,
+  history,
+  locationArrow,
+  trucks,
+  paymentCard,
+  wordmark,
+}
--- apps/frontend/src/components/primitives/nav/nav-item.tsx
+++ apps/frontend/src/components/primitives/nav/nav-item.tsx
@@ -1,21 +1,29 @@
 import Link from "next/link"
 import { Icon, IconName } from "../icon"
+import { twMerge } from "tailwind-merge"
 export interface NavItemProps {
   isActive?: boolean
   icon?: IconName
+  disableHover?: boolean
   label?: string
   href: string
 }
 
-export function NavItem({ icon, label, href, isActive }: NavItemProps) {
+export function NavItem({
+  icon,
+  label,
+  href,
+  isActive,
+  disableHover,
+}: NavItemProps) {
   console.log("The current path is: ", href, isActive)
+  const className = twMerge(
+    `inline-flex px-4 py-2 items-center justify-start rounded-lg transition ease-in-out`,
+    isActive ? "bg-slate-3" : "bg-white",
+    disableHover ? undefined : "hover:bg-slate-4"
+  )
   return (
-    <Link
-      className={`inline-flex px-4 py-2 items-center justify-start transition ease-in-out  hover:bg-slate-5 ${
-        isActive ? "bg-slate-3" : "bg-white"
-      } rounded-lg`}
-      href={href}
-    >
+    <Link className={className} href={href}>
       <div className="w-4 h-4">
         {icon && <Icon name={icon} className="w-4 h-4" />}
       </div>
