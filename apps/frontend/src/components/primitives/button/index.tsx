a5a3c6454320c6acb395c5fc681d619978818446
Gregg Luskin <gdluskin@gmail.com>
2024-02-07 17:30:05 -0800

    working

--- apps/frontend/src/components/primitives/button/index.tsx
+++ apps/frontend/src/components/primitives/button/index.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/tailwind/utils"
 import { Icon, IconName } from "../icon"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 transition",
   {
     variants: {
       variant: {
