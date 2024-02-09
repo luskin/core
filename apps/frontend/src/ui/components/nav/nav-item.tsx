5db155ab6094ea891bc4dd2e60b0f978270ed803
Gregg Luskin <gdluskin@gmail.com>
2024-02-08 16:27:49 -0800

    more forward refs

--- apps/frontend/src/ui/components/nav/nav-item.tsx
+++ apps/frontend/src/ui/components/nav/nav-item.tsx
@@ -2,42 +2,45 @@ import Link from "next/link"
 import { Icon, IconName } from "../icon"
 import { twMerge } from "tailwind-merge"
 import { Paragraph } from "../typography"
-export interface NavItemProps {
-  isActive?: boolean
+import React from "react"
+
+export interface NavItemProps
+  extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  asChild?: boolean
   icon?: IconName
-  disableHover?: boolean
   label?: string
   href: string
+  isActive?: boolean
+  disableHover?: boolean
 }
 
-export function NavItem({
-  icon,
-  label,
-  href,
-  isActive,
-  disableHover,
-}: NavItemProps) {
-  const className = twMerge(
-    `inline-flex px-4 py-2 w-full items-center justify-start rounded-lg transition ease-in-out text-slate-6`,
-    isActive ? "bg-slate-3 text-slate-12" : "bg-white",
-    disableHover ? undefined : "hover:bg-slate-12 hover:text-slate-1"
-  )
-  return (
-    <Link className={className} href={href}>
-      <div className="w-4 h-4">
-        {icon && (
-          <Icon
-            name={icon}
-            className={twMerge(
-              "w-4 h-4",
-              isActive ? "text-slate-12" : "text-slate-6"
-            )}
-          />
-        )}
-      </div>
-      <Paragraph size={"md"} variant={"secondary"} className="ml-2">
-        {label}
-      </Paragraph>
-    </Link>
-  )
-}
+const NavItem = React.forwardRef<HTMLAnchorElement, NavItemProps>(
+  ({ icon, label, href, isActive, disableHover, ...props }, ref) => {
+    const className = twMerge(
+      `inline-flex px-4 py-2 w-full items-center justify-start rounded-lg transition ease-in-out text-slate-6`,
+      isActive ? "bg-slate-3 text-slate-12" : "bg-white",
+      disableHover ? undefined : "hover:bg-slate-12 hover:text-slate-1"
+    )
+    return (
+      <Link className={className} href={href} ref={ref} {...props}>
+        <div className="w-4 h-4">
+          {icon && (
+            <Icon
+              name={icon}
+              className={twMerge(
+                "w-4 h-4",
+                isActive ? "text-slate-12" : "text-slate-6"
+              )}
+            />
+          )}
+        </div>
+        <Paragraph size={"md"} variant={"secondary"} className="ml-2">
+          {label}
+        </Paragraph>
+      </Link>
+    )
+  }
+)
+NavItem.displayName = "NavItem"
+
+export { NavItem }
