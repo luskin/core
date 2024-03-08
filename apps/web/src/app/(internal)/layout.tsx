a721f92f198fa5257913b269ec7f630e9c0ebea0
Gregg Luskin <gdluskin@gmail.com>
2024-03-08 12:28:53 -0800

    fix build

--- apps/web/src/app/(internal)/layout.tsx
+++ apps/web/src/app/(internal)/layout.tsx
@@ -1,15 +1,6 @@
 import { AdminBoundary } from '@/ui/boundaries/admin.boundary';
 import { PropsWithChildren } from 'react';
-import AppBarLayout from '../_layouts/app-bar.layout';
-import AppBar from '../@appbar/layout';
-import { Footer } from '@/ui/layout/footer';
 
 export default function DesignLayout(props: PropsWithChildren) {
-  return (
-    <AdminBoundary redirectOnFailHref="/not-found">
-      <AppBarLayout appBar={<AppBar />} footer={<Footer />}>
-        {props.children}
-      </AppBarLayout>
-    </AdminBoundary>
-  );
+  return <AdminBoundary redirectOnFailHref="/not-found">{props.children}</AdminBoundary>;
 }
